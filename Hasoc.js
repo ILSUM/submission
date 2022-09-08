342a0cc3caedf09647408ae2d5c202c68023e548
Shrey Satapara <shrey.satapara.99@gmail.com>
2022-09-08 16:57:22 +0530

    typo fiexd

--- Hasoc.js
+++ Hasoc.js
@@ -1,5 +1,5 @@
-proxy = `http://127.0.0.1:5000/`
-    //proxy = `https://ilsumsub.el.r.appspot.com`
+//proxy = `http://127.0.0.1:5000/`
+proxy = `https://ilsumsub.el.r.appspot.com`
 
 var task_titles = {
     "English": "English",
